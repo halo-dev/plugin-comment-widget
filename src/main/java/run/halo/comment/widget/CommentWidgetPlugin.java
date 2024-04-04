2ef099cb1172eaa240beaeba947766b4e70b3ce4
mashirot <shiina@sakurasou.io>
2024-04-04 22:32:54 +0800

    revert: feat: add setting model

--- src/main/java/run/halo/comment/widget/CommentWidgetPlugin.java
+++ src/main/java/run/halo/comment/widget/CommentWidgetPlugin.java
@@ -1,9 +1,7 @@
 package run.halo.comment.widget;
 
-import lombok.RequiredArgsConstructor;
 import org.pf4j.PluginWrapper;
 import org.springframework.stereotype.Component;
-import run.halo.app.extension.SchemeManager;
 import run.halo.app.plugin.BasePlugin;
 
 /**
@@ -12,21 +10,15 @@ import run.halo.app.plugin.BasePlugin;
  */
 @Component
 public class CommentWidgetPlugin extends BasePlugin {
-
-    public final SchemeManager schemeManager;
-
-    public CommentWidgetPlugin(PluginWrapper wrapper, SchemeManager schemeManager) {
+    public CommentWidgetPlugin(PluginWrapper wrapper) {
         super(wrapper);
-        this.schemeManager = schemeManager;
     }
 
     @Override
     public void start() {
-        schemeManager.register(CommentWidgetSetting.class);
     }
 
     @Override
     public void stop() {
-        schemeManager.unregister(schemeManager.get(CommentWidgetSetting.class));
     }
 }
