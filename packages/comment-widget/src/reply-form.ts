d49dbeae213f0a5b9e3ca2dce6481d042357517a
Ryan Wang <i@ryanc.cc>
2024-05-04 22:45:29 +0800

    feat: disable button when submitting comments (#120)
    支持在提交评论或回复时，禁用提交按钮，防止重复提交。
    
    Fixes https://github.com/halo-dev/plugin-comment-widget/issues/116
    
    <img width="641" alt="image" src="https://github.com/halo-dev/plugin-comment-widget/assets/21301288/9dc636ea-8631-4be6-9386-e8e0b19dd433">
    
    /kind feature
    
    ```release-note
    支持在提交评论或回复时，禁用提交按钮，防止重复提交。
    ```

--- packages/comment-widget/src/reply-form.ts
+++ packages/comment-widget/src/reply-form.ts
@@ -1,7 +1,7 @@
 import './base-form';
 import { CommentVo, Reply, ReplyRequest, ReplyVo, User } from '@halo-dev/api-client';
-import { LitElement, html } from 'lit';
-import { Ref, createRef, ref } from 'lit/directives/ref.js';
+import { html, LitElement } from 'lit';
+import { createRef, Ref, ref } from 'lit/directives/ref.js';
 import {
   allowAnonymousCommentsContext,
   baseUrlContext,
@@ -36,6 +36,9 @@ export class ReplyForm extends LitElement {
   @state()
   toastManager: ToastManager | undefined;
 
+  @state()
+  submitting = false;
+
   baseFormRef: Ref<BaseForm> = createRef<BaseForm>();
 
   override connectedCallback(): void {
@@ -48,11 +51,18 @@ export class ReplyForm extends LitElement {
   }
 
   override render() {
-    return html`<base-form ${ref(this.baseFormRef)} @submit="${this.onSubmit}"></base-form>`;
+    return html` <base-form
+      .submitting=${this.submitting}
+      ${ref(this.baseFormRef)}
+      @submit="${this.onSubmit}"
+    ></base-form>`;
   }
 
   async onSubmit(e: CustomEvent) {
     e.preventDefault();
+
+    this.submitting = true;
+
     const data = e.detail;
 
     const { displayName, email, website, content } = data || {};
@@ -70,12 +80,14 @@ export class ReplyForm extends LitElement {
 
     if (!this.currentUser && !this.allowAnonymousComments) {
       this.toastManager?.warn('请先登录');
+      this.submitting = false;
       return;
     }
 
     if (!this.currentUser && this.allowAnonymousComments) {
       if (!displayName || !email) {
         this.toastManager?.warn('请先登录或者完善信息');
+        this.submitting = false;
         return;
       } else {
         replyRequest.owner = {
@@ -116,6 +128,8 @@ export class ReplyForm extends LitElement {
       if (error instanceof Error) {
         this.toastManager?.error(error.message);
       }
+    } finally {
+      this.submitting = false;
     }
   }
 }
