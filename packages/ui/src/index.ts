4453ffbe98c9b43d98f53b344a5a1ea47a132e98
Ryan Wang <i@ryanc.cc>
2026-09-09 18:09:37 +0800

    fix: preserve comment provider component identity

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,27 +2,30 @@ import { VLoading } from '@halo-dev/components';
 import { definePlugin } from '@halo-dev/ui-shared';
 import { defineAsyncComponent, markRaw } from 'vue';
 
+const Editor = markRaw(
+  defineAsyncComponent({
+    loader: () => import('./components/Editor.vue'),
+    loadingComponent: VLoading,
+  })
+);
+const Content = markRaw(
+  defineAsyncComponent({
+    loader: () => import('./components/Content.vue'),
+  })
+);
+
 export default definePlugin({
   components: {},
   routes: [],
   extensionPoints: {
     'comment:editor:replace': () => {
       return {
-        component: markRaw(
-          defineAsyncComponent({
-            loader: () => import('./components/Editor.vue'),
-            loadingComponent: VLoading,
-          })
-        ),
+        component: Editor,
       };
     },
     'comment:list-item:content:replace': () => {
       return {
-        component: markRaw(
-          defineAsyncComponent({
-            loader: () => import('./components/Content.vue'),
-          })
-        ),
+        component: Content,
       };
     },
   },
